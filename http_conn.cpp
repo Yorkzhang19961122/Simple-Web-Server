0a09e388ccb63347096df6728d98a2adbf77d373
yorkzhang <15257379410@163.com>
2021-07-09 20:53:32 +0800

    prase http request

--- http_conn.cpp
+++ http_conn.cpp
@@ -17,7 +17,8 @@ void setnonblocking(int fd) {
 void addfd(int epollfd, int fd, bool one_shot) {
     epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN | EPOLLRDHUP;
+    // event.events = EPOLLIN | EPOLLRDHUP;
+    event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
 
     if(one_shot) {
         event.events | EPOLLONESHOT;
@@ -54,6 +55,25 @@ void http_conn::init_conn(int sockfd, const sockaddr_in& addr) {
     addfd(m_epollfd, sockfd, true);
     /*总用户数加1*/
     m_user_count++;
+
+    init();
+}
+
+void http_conn::init() {
+    /*初始化状态为解析请求首行*/
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_checked_index = 0;
+    m_start_line = 0;
+    m_read_idx = 0;
+    
+    m_method = GET;
+    m_url = 0;
+    m_version = 0;
+    m_host = 0;
+    m_linger = false;
+    
+
+    bzero(m_read_buf, READ_BUFFER_SIZE);
 }
 
 /*关闭连接*/
@@ -67,8 +87,29 @@ void http_conn::close_conn() {
 }
 
 /*非阻塞的读*/
+/*循环读取客户的数据直到无数据可读*/
 bool http_conn::read() {
-    printf("一次性读完数据\n");
+    /*对本程序来说可有可无*/
+    if(m_read_idx >= READ_BUFFER_SIZE) {
+        return false;
+    }
+    /*读取到的字节*/
+    int bytes_read = 0;
+    while(true) {
+        bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+        if(bytes_read == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                /*没有数据了*/
+                break;
+            }
+            return false;
+        } else if(bytes_read == 0) {
+            /*对方关闭连接*/
+            return false;
+        }
+        m_read_idx += bytes_read;
+    }
+    printf("读取到了数据: %s\n", m_read_buf);
     return true;
 }
 
@@ -81,7 +122,169 @@ bool http_conn::write() {
 /*由线程池中的工作线程调用的，这是处理HTTP请求的入口函数*/
 void http_conn::process() {
     /*解析HTTP请求*/
-    printf("parse request, create reponse\n");
+    HTTP_CODE read_ret = process_read();
+    /*请求不完整*/
+    if(read_ret == NO_REQUEST) {
+        modfd(m_epollfd, m_sockfd, EPOLLIN);
+        return;
+    }
+
     /*生成响应*/
+    
+}
+
+
+/*主状态机，解析请求*/
+http_conn::HTTP_CODE http_conn::process_read() {
+    
+    LINE_STATUS line_status= LINE_OK;
+    HTTP_CODE ret = NO_REQUEST;
+
+    char* text = 0;
+
+    /*
+        (主状态机解析请求体 && 从状态机检查的一行OK) || 解析到一行完整的数据OK
+        即：解析到了请求体且是完整的数据 或者 解析到了一行完整的数据        
+    */
+    while(((m_check_state == CHECK_STATE_CONTENT) && (line_status == LINE_OK))
+            ||((line_status = parse_line()) == LINE_OK)) {
+        /*获取一行数据*/
+        text = get_line();
+
+        m_start_line = m_checked_index;
+        printf("got 1 http line : %s", text);
+
+        switch(m_check_state) {
+            case CHECK_STATE_REQUESTLINE: {
+                ret = parse_request_line(text);
+                if(ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                }
+                break;
+            }
+            case CHECK_STATE_HEADER: {
+                ret = parse_headers(text);
+                if(ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                } else if(ret == GET_REQUEST) {
+                    return do_request();
+                }
+            }
+            case CHECK_STATE_CONTENT: {
+                ret = parse_content(text);
+                if(ret == GET_REQUEST) {
+                    return do_request();
+                }
+                /*行数据不完整*/
+                line_status = LINE_OPEN;
+                break;
+            }
+            default: {
+                return INTERNAL_ERROR;
+            } 
+        }
+        return NO_REQUEST;
+    }
+
+    return NO_REQUEST;
+}
+/*
+    解析请求首行(请求行)，获得请求方法，目标URL，HTTP版本
+    text = "GET /index.html HTTP/1.1"    
+*/
+http_conn::HTTP_CODE http_conn::parse_request_line(char* text) {
+    
+    m_url = strpbrk(text, " \t");
+    /*    
+        m_url = " /index.html HTTP/1.1"
+        text  = "GET /index.html HTTP/1.1"    
+    */  
+
+
+    *m_url++ = '\0';
+    /*    
+        m_url = "/index.html HTTP/1.1"    
+        text  = "GET\0/index.html HTTP/1.1" 
+    */  
+
+    char* method = text;
+    /*
+        method = "GET"
+    */
+
+    if(strcasecmp(method, "GET") == 0) {
+        m_method = GET;
+    } else {
+        return BAD_REQUEST;
+    }
+
+    m_version = strpbrk(m_url, " \t");
+    /*
+        m_version = " HTTP/1.1"
+        m_url     = "/index.html HTTP/1.1"
+    */
+
+    if(!m_version) {
+        return BAD_REQUEST;
+    }
+
+    *m_version++ = '\0';
+    /*
+        m_version = "HTTP/1.1"
+        m_url     = "/index.html\0HTTP/1.1"
+    */
+
+    if(strcasecmp(m_version, "HTTP/1.1") != 0) {
+        return BAD_REQUEST;
+    }
+    /*去除"http://"的影响*/
+    /* 此时使用m_url都已经变成了"/index.html" */
+    if(strncasecmp(m_url, "http://", 7) == 0) {
+        m_url += 7;
+        m_url = strchr(m_url, '/');
+    }
+    if(!m_url || m_url[0] != '/') {
+        return BAD_REQUEST;
+    }
+    /*主状态机的检查状态变成检查请求头*/
+    m_check_state = CHECK_STATE_HEADER;
+    return NO_REQUEST;
+
+}
+/*解析请求头*/
+http_conn::HTTP_CODE http_conn::parse_headers(char* text) {
+
+}
+/*解析请求体*/
+http_conn::HTTP_CODE http_conn::parse_content(char* text) {
+
+}
+/*解析(获取)一行，判断依据\r\n*/
+http_conn::LINE_STATUS http_conn::parse_line() {
+    char temp;
+    for( ; m_checked_index < m_read_idx ; ++m_checked_index) {
+        temp = m_read_buf[m_checked_index];
+        if(temp == '\r') {
+            if((m_checked_index + 1) == m_read_idx) {
+                return LINE_OPEN;
+            } else if(m_read_buf[m_checked_index + 1] == '\n') {
+                m_read_buf[m_checked_index++] = '\0';
+                m_read_buf[m_checked_index++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        } else if(temp == '\n') {
+            if((m_checked_index > 1) && (m_read_buf[m_checked_index - 1] == '\r')) {
+                m_read_buf[m_checked_index - 1] = '\0';
+                m_read_buf[m_checked_index++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+        return LINE_OPEN;
+    }
+}
+
+http_conn::HTTP_CODE http_conn::do_request() {
     
 }
